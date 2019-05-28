04a387017ce3b481402df5ceb76b3244df464220
Samantha Kellow <sammii.h@icloud.com>
2019-05-28 20:30:47 +0100

    login route tested and working

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ db = Database()
 
 db.bind('postgres', 'postgres://localhost:5432/artistry-db')
 
-# from models.User import User, UserSchema
 # pylint disable=W0611,C0413
 from config import routes
 
@@ -16,7 +15,3 @@ db.generate_mapping(create_tables=True)
 @db_session
 def home():
     return 'Hello World!', 200
-    # schema = UserSchema(many=True)
-    # users = User.select()
-    # print(users)
-    # return schema.dumps(users)
