0729623551133104568d35ea774bea622e6afda5
Samantha Kellow <sammii.h@icloud.com>
2019-05-28 20:29:34 +0100

    registration route up and running

--- app.py
+++ app.py
@@ -4,15 +4,19 @@ from pony.orm import Database, db_session
 app = Flask(__name__)
 db = Database()
 
-db.bind(provider='postgres', database='artistry-db')
-from models.User import User, UserSchema
+db.bind('postgres', 'postgres://localhost:5432/artistry-db')
+
+# from models.User import User, UserSchema
+# pylint disable=W0611,C0413
+from config import routes
 
 db.generate_mapping(create_tables=True)
 
 @app.route('/', methods=['GET'])
 @db_session
 def home():
-    schema = UserSchema(many=True)
-    users = User.select()
-    print(users)
-    return schema.dumps(users)
+    return 'Hello World!', 200
+    # schema = UserSchema(many=True)
+    # users = User.select()
+    # print(users)
+    # return schema.dumps(users)
