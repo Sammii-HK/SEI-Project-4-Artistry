7f9752051a6780226c927430eb2467df0de968b7
Samantha Kellow <sammii.h@icloud.com>
2019-06-03 09:14:04 +0100

    search functionality working but needs further testing

--- controllers/rijksmuseum.py
+++ controllers/rijksmuseum.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from flask import Blueprint, Response
+from flask import Blueprint, Response, request
 
 
 router = Blueprint('rijks', __name__)
@@ -40,13 +40,15 @@ def item(objectNumber):
 # this url returns a working search query in insomnia
 # https://www.rijksmuseum.nl/api/en/collection/?q=still%20life&key={{ api_key  }}&format=json
 
-@router.route('/rijksmuseum/collection?q=<query>')
-def search(query):
+@router.route('/rijksmuseum/collection')
+def search():
+    query = request.args.get('query')
     api_key = os.getenv('RIJKS_API_KEY')
-    url = f'https://www.rijksmuseum.nl/api/en/collection/?q={query}'
+    url = 'https://www.rijksmuseum.nl/api/en/collection'
     params = {
+        'q': query,
         'key': api_key
-        }
+    }
     req = requests.get(url, params=params)
 
     print('req.status_code', req.status_code)
