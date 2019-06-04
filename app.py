8f0166f57f09875a264b5748120d200abdbea72d
Samantha Kellow <sammii.h@icloud.com>
2019-06-04 13:36:35 +0100

    deployment to heroku

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from pony.orm import Database
+from config.environment import db_uri
 
 app = Flask(__name__, static_folder='public')
 db = Database()
-db.bind('postgres', 'postgres://localhost:5432/artistry-db')
+db.bind('postgres', db_uri)
 
 # pylint: disable=W0611,C0413
 from config import routes
